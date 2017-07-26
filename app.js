c48f7c861a6effecc8c5edc7be2ae282f6b3c782
wiltrahan <twilorip@gmail.com>
2017-07-25 20:06:29 -0400

    completed walkthrough

--- app.js
+++ app.js
@@ -9,34 +9,40 @@ $(document).ready(function() {
     data: data,
     type: "GET",
     success: function(response) {
-      // console.log(response);
-      // console.log(response.sources[2]);
       var sources = response.sources;
-      // console.log(sources[2]);
-      var html = "<select class='form-control' id='source'>";
+      var html = "<select class='form-control' id='sourceOption'>";
       $.each(sources, function(index, source) {
-        // console.log(source.id);
-        html += "<option value=" + index + ">" + source.name + "</option>";
+        html += "<option value='" + source.id + "'>" + source.name + "</option>";
       })
       html += "</select>";
       $(".form-group").html(html);
     }
   });
 
+  $("#source").submit(function(event) {
+    event.preventDefault();
+    var id = $("#sourceOption").val();
+    var url = "https://newsapi.org/v1/articles";
+    var data = {
+        apiKey: "ce9645ec5df84064aa5200fb2087c255",
+        source: id
+      };
+    $.ajax({
+      url: url,
+      data: data,
+      type: "GET",
+      success: function(response) {
+        var articles = response.articles;
+        var html = "<ul class='list-group'>";
 
-  var articleUrl = "https://newsapi.org/v1/articles";
-  var articleData = {
-    source: ""
-  }
+        $.each(articles, function(index, article) {
+          html += "<li class='list-item'>" + article.title + "</li>";
+        })
+        html += "</ul>";
+        $("#articles").html(html);
+      }
+    });
+  })
+
 });
 
-// $('input:button').click(function() {
-//     alert($(this).val());
-// });​
-
-//after user selects newsSource
-//have list of links from source show up on page
-//going to need second API call
-//send ID
-//git init this
-//branch off master
